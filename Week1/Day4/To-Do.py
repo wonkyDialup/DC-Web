bf7a959b33cbf0b6c4927a2e88f783dcd8bb54bc
Justin Gibbs <justin.gibbs59@gmail.com>
2021-08-05 21:08:29 -0400

    Todo list homework

---
+++ Week1/Day4/To-Do.py
@@ -0,0 +1,16 @@
+# To Do List
+print("Welcome, please select an option")
+taskSelect = int(input("Select one of the following:\n\n1. Add a task\n\n2. Delete a task\n\n3. View all tasks\n\n Press Q to quit\n\n"))
+if taskSelect == 1:
+    taskName = input("What is the name of the task?\n")
+    priority = input("What is the priority of your task?\n\nHigh\nMedium\nLow\n")
+    task_dict = {taskName, priority}
+def getTask(task_dict):
+    tasks = []
+    for key in task_dict:
+        tasks.append(key)
+
+    return tasks
+items = task_dict.items()
+
+list(newTask.key())
